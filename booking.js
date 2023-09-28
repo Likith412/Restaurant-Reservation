92bdb7f37385c06660fbb7b26e627b2fa9506465
Likith412 <137510073+Likith412@users.noreply.github.com>
2023-09-28 20:40:04 +0530

    Update booking.js

--- booking.js
+++ booking.js
@@ -1,11 +1,12 @@
 const checkboxes = document.getElementsByClassName('myCheckbox');
-        
-for (let i = 0; i < checkboxes.length; i++) {
-    const checkbox = checkboxes[i];
-    
-    checkbox.addEventListener('change', function () {
-        if (!checkbox.checked) {
-            checkbox.removeAttribute('name');
-        }
-    });
-}
+const submit= document.getElementById('sub');
+submit.addEventListener('click',()=>{
+    for (let i = 0; i < checkboxes.length; i++) {
+        const checkbox = checkboxes[i];
+       
+            if (!checkbox.checked) {
+                checkbox.removeAttribute('name');
+            }
+      
+    }
+});
